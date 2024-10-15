67f0a031703d2a212d72c1bfc010f105d3951267
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-10-15 18:48:55 +0100

    feat: implement auth flow

--- server.ts
+++ server.ts
@@ -1,9 +1,24 @@
 import express from "express"
 import { getPayloadClient } from "./get-payload"
 import { nextHandler, nextApp } from "./next-utils"
+import * as trpcExpress from "@trpc/server/adapters/express"
+import { appRouter } from "./trpc"
+import { inferAsyncReturnType } from '@trpc/server'
+
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
+const createContext = ({
+    req,
+    res,
+  }: trpcExpress.CreateExpressContextOptions) => ({
+    req,
+    res,
+  })
+
+  export type ExpressContext = inferAsyncReturnType<
+  typeof createContext
+>
 
 const start = async () => {
     const payload = await getPayloadClient({
@@ -14,6 +29,13 @@ const start = async () => {
             },
         },
     })
+
+
+   app.use('/api/trpc', 
+    trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext
+   })) 
 
    app.use((req, res) => nextHandler(req, res))
 
