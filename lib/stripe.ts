e4c8154ab9007e5953246657249dc084fe9d60a1
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-10-16 02:37:01 +0100

    bug(fix): resolved strip bug

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 export const stripe = new Stripe(
   process.env.STRIPE_SECRET_KEY ?? '',
   {
-    apiVersion: '2024-10-15',
+    apiVersion: '2024-09-30',
     typescript: true,
   }
 )
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -8,7 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: 'NGN' | 'USD' | 'EUR' | 'GBP' | 'USDT'
+    currency?: 'USD' | 'EUR' | 'GBP' | 'USDT'
     notation?: Intl.NumberFormatOptions['notation']
   } = {}
 ) {
@@ -23,4 +24,47 @@ export function formatPrice(
     notation,
     maximumFractionDigits: 2,
   }).format(numericPrice)
+}
+
+export function constructMetadata({
+  title = 'DevHubs - the marketplace for awesome projects',
+  description = 'DevHubs is a marketplace for high-quality projects.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@code_with_Felix',
+    },
+    icons,
+    metadataBase: new URL('https://devhubs.vercel.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
