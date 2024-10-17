fd4787ba031402a2d05f54963f6ad2a4844747fe
Happy Felix Chukwuma <happychukwuma@gmail.com>
2024-10-17 06:05:33 +0100

    chore(fix) restructured project layout and db schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,32 @@
-import Navbar from '@/components/Navbar'
-import Providers from '@/components/Providers'
-import { cn, constructMetadata } from '@/lib/utils'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Toaster } from 'sonner'
-import './globals.css'
-import Footer from '@/components/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Navbar } from "./components/Navbar";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
+import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = constructMetadata()
+export const metadata: Metadata = {
+  title: "DevHubs",
+  description: "A platform for developers/techies to showcase, share, and monetize their projects.",
+};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang='en' className='h-full'>
-      <body
-        className={cn(
-          'relative h-full font-sans antialiased',
-          inter.className
-        )}>
-        <main className='relative flex flex-col min-h-screen'>
-          <Providers>
-            <Navbar />
-            <div className='flex-grow flex-1'>
-              {children}
-            </div>
-            <Footer />
-          </Providers>
-        </main>
-
-        <Toaster position='top-center' richColors />
+    <html lang="en">
+      <body className={inter.className}>
+        <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
+        <Navbar />
+        {children}
+        <Toaster richColors theme="light" closeButton />
       </body>
     </html>
-  )
+  );
 }
